c084b62277433e8518e6ea2e243b4b6d2c5bc442
Frank <00frankgarcia@gmail.com>
2024-03-14 00:37:26 -0300

    fix in sockets connection

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? 'http://localhostinger:3002' : 'http://localhost:3002';
+const URL = process.env.NODE_ENV === 'production' ? import.meta.env.VITE_WS_URL : 'http://localhost:3002';
 
 export const socket = io(URL, {
   autoConnect: false
