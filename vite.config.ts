8cce556bb17c7325728bb1b9a470160315aa4e55
Frank <00frankgarcia@gmail.com>
2024-03-15 23:06:02 -0300

    adding new configuration to add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
@@ -14,5 +14,38 @@ export default defineConfig({
       '@types': path.resolve(__dirname, './src/types/types.d.ts'),
       '@socket': path.resolve(__dirname, './src/socket/socket.ts'),
     }
-  }
+  },
+  plugins: [react(),
+    VitePWA({
+      manifest: {
+        display: 'standalone',
+        display_override: ['window-controls-overlay'],
+        lang: 'es-ES',
+        name: 'Youtube Clone',
+        short_name: 'YT Clone',
+        description: 'Aplicacion clon de youtube hecha con react + tailwind + vite',
+        theme_color: '#18181b',
+        background_color: '#18181b',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+        ]
+      }
+    })
+  ]
 })
