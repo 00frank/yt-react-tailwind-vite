521bba1b7d6768837dd7a974789a54a6aaf0cac2
Frank <00frankgarcia@gmail.com>
2024-03-15 22:30:23 -0300

    using pwa prompt for ios

---
+++ src/contants.ts
@@ -0,0 +1,8 @@
+export const PWAPromptProps = {
+  copyTitle: 'Añadir a la pantalla principal',
+  copyBody: 'Esta pagína puede transformarse en aplicación. Añadela a tu pagina principal para usarla en pantalla completa y de manera offline.',
+  copyShareButtonLabel: '1) Haz clic en el botón "Compartir"',
+  copyAddHomeButtonLabel: '2) Luego selecciona "Agregar a Inicio"',
+  copyClosePrompt: 'Cerrar',
+  copyShareButton: 'Compartir',
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Toaster } from 'sonner'
+import { PwaPrompt as PWAPrompt } from 'react-ios-pwa-prompt-ts'
+import { PWAPromptProps } from './contants.ts'
 
 import App from './App.tsx'
 
@@ -9,6 +11,9 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <PWAPrompt
+      {...PWAPromptProps}
+    />
     <UserProvider>
       <Toaster position="top-center" richColors closeButton />
       <App />
